2fe0f5c73be2b6af6a2f642281a1f0debf9a1f49
meryem2137 <116557318+meryem2137@users.noreply.github.com>
2023-02-05 19:33:33 +0300

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+public class fibonacci{
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a=0,b=1;
+        int c,n;
+        System.out.print("Fibonacci serisinin eleman sayısını giriniz: ");
+        n=input.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.print(a+" " );
+            c=a+b;
+            a=b;
+            b=c;
+        }
+
+
+
+
+
+
+
+
+
+
+
+        }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//www.patika.dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
